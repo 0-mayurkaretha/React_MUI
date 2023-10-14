1d94f3b36440a4bacf23f7bf468020aee65fe509
0-mayurkaretha <0.mayurkaretha@gmail.com>
2023-10-14 19:36:59 +0530

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import './App.css';
 import MuiTypography from './components/MuiTypography';
 
 function App() {
   return (
     <div className="App">
-      <MuiTypography/>
+      <BrowserRouter>
+        <button><Link to="/">Home</Link></button>&nbsp;&nbsp;
+        <button><Link to="/MuiTypography">MuiTypography</Link></button>&nbsp;&nbsp;
+        <Routes>
+          <Route path="/MuiTypography" element={<MuiTypography />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
