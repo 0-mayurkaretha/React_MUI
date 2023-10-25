3d97ebf2d097956bcbf937cc2f73e8a7a21e67e7
0-mayurkaretha <0.mayurkaretha@gmail.com>
2023-10-25 09:45:56 +0530

    Badge

--- src/components/MuiDataDisplay.tsx
+++ src/components/MuiDataDisplay.tsx
@@ -1,11 +1,13 @@
 import React from "react"
-import { Stack, Avatar, AvatarGroup } from '@mui/material'
+import { Stack, Avatar, AvatarGroup, Badge } from '@mui/material'
+import MailIcon from '@mui/icons-material/Mail'
 
 const MuiDataDisplay = () => {
   return (
     <div style={{paddingTop:"32px"}}>
       
-      <Stack spacing={4}>
+      Avatar: <br/><br/>
+      <Stack spacing={2}>
         <Stack direction='row' spacing={1}>
           <Avatar sx={{ bgcolor: 'primary.light'}}>BW</Avatar>
           <Avatar sx={{ bgcolor: 'success.light'}}>CK</Avatar>
@@ -18,6 +20,29 @@ const MuiDataDisplay = () => {
             <Avatar src='https://randomuser.me/api/portraits/men/51.jpg' alt='john doe' />
           </AvatarGroup>
         </Stack>
+      </Stack>
+
+      <br/><br/>
+      Badge: <br/><br/>
+      <Stack spacing={2} direction='row'>
+        <Badge badgeContent={5}  color='primary'>
+          <MailIcon />
+        </Badge>
+        <Badge badgeContent={0}  color='primary'>
+          <MailIcon />
+        </Badge>
+        <Badge badgeContent={5}  color='secondary'>
+          <MailIcon />
+        </Badge>
+        <Badge badgeContent={100}  color='error'>
+          <MailIcon />
+        </Badge>
+        <Badge badgeContent={1000}  color='error' max={999}>
+          <MailIcon />
+        </Badge>
+        <Badge variant='dot'  color='primary'>
+          <MailIcon />
+        </Badge>
       </Stack>
     </div>
   )
